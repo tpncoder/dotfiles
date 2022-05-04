99fb9fb240f68b70fa1a0193f36fe8e5a9812e63
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-05-04 19:27:39 +0530

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -103,34 +103,30 @@ require('lualine').setup {
 	options = {
     		icons_enabled = true,
     		theme = 'auto',
-    		component_separators = { left = '', right = ''},
+    		component_separators = { left = '', right = '|'},
     		section_separators = { left = '', right = ''},
     		disabled_filetypes = {},
     		always_divide_middle = true,
     		globalstatus = false,
   	},
   	sections = {
-    		lualine_a = {'mode'},
-    		lualine_b = {'branch', 'diff', 'diagnostics'},
+    		lualine_a = {},
+    		lualine_b = {{ 'mode', separator = { left = '', right = '' }, right_padding = 2 },},
     		lualine_c = {'filename'},
-    		lualine_x = {'encoding', 'filetype'},
-    		lualine_y = {'location'},
-    		lualine_z = {''}
-  	},
+    		lualine_x = {''},
+    		lualine_y = {'filetype', 'progress'},
+    		lualine_z = {{ 'location', separator = { right = '' }, left_padding = 2 },},},
   	inactive_sections = {
     		lualine_a = {},
     		lualine_b = {},
-    		lualine_c = {'filename'},
-    		lualine_x = {'progress'},
+    		lualine_c = {▊},
+    		lualine_x = {},
     		lualine_y = {},
     		lualine_z = {}
  	},
   		tabline = {},
  		extensions = {}
 		}
-
-
-
 
 
 
