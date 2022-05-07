6d33e6706cf1473b890afcacaacc0b496bdc90b3
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-05-07 15:49:54 +0530

    Update init.lua

--- nvim/lua/bufferline/init.lua
+++ nvim/lua/bufferline/init.lua
@@ -1,18 +1,50 @@
-require("tabline").setup{
-	-- Defaults configuration options
-      enable = true,
-      options = {
-      -- If lualine is installed tabline will use separators configured in lualine by default.
-      -- These options can be used to override those settings.
-        section_separators = {'', ''},
-        component_separators = {'', ''},
-        max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
-        show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
-        show_devicons = true, -- this shows devicons in buffer section
-        show_bufnr = false, -- this appends [bufnr] to buffer section,
-        show_filename_only = false, -- shows base filename only instead of relative path in filename
-        modified_icon = "+ ", -- change the default modified icon
-        modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
-        show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
-      }
-}
+local get_hex = require('cokeline/utils').get_hex
+
+require('cokeline').setup({
+  default_hl = {
+    fg = function(buffer)
+      return
+        buffer.is_focused
+        and get_hex('Normal', 'fg')
+         or get_hex('Comment', 'fg')
+    end,
+    bg = get_hex('ColorColumn', 'bg'),
+  },
+
+  components = {
+    {
+      text = '| ',
+      bg = get_hex('Normal', 'bg'),
+    },
+    {
+      text = '',
+      fg = get_hex('ColorColumn', 'bg'),
+      bg = get_hex('Normal', 'bg'),
+    },
+    {
+      text = function(buffer)
+        return buffer.devicon.icon
+      end,
+      fg = function(buffer)
+        return buffer.devicon.color
+      end,
+    },
+    {
+      text = ' ',
+    },
+    {
+      text = function(buffer) return buffer.filename .. '  ' end,
+      style = function(buffer)
+        return buffer.is_focused and 'bold' or nil
+      end,
+    },
+    {
+      text = '',
+      delete_buffer_on_left_click = true,
+    },
+    {
+      text = '',
+
+    },
+  },
+})
