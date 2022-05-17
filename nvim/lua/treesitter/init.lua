e053c5939e3c13cff5970f4a552a94bde4e7276c
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-05-17 19:57:18 +0530

    Update init.lua

--- nvim/lua/treesitter/init.lua
+++ nvim/lua/treesitter/init.lua
@@ -1,3 +1,7 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
 require"nvim-treesitter.configs".setup {
   ensure_installed = { "c", "lua", "rust" },
   sync_install = true,
