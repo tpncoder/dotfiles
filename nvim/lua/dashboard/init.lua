f25aa94019bf854d8599a1077a12f40d758a8cd3
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-05-17 19:49:39 +0530

    Update init.lua

--- nvim/lua/dashboard/init.lua
+++ nvim/lua/dashboard/init.lua
@@ -1,3 +1,7 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+	return
+end
 require('alpha').setup(require'alpha.themes.dashboard'.config)
 local present, alpha = pcall(require, "alpha")
 
