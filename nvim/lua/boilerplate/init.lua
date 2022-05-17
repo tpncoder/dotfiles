6f96959177c0ec3e0f33fbd381eae5a64a8dd16d
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-05-17 19:44:32 +0530

    Update init.lua

--- nvim/lua/boilerplate/init.lua
+++ nvim/lua/boilerplate/init.lua
@@ -1,1 +1,1 @@
-vim.cmd[[autocmd BufNewFile *.html 0r C:/Users/TEJAS/AppData/Local/nvim/lua/templates/html.skel]]
+vim.cmd[[autocmd BufNewFile *.html 0r C:/Users/TEJAS/AppData/Local/nvim/lua/boilerplate/html.skel]]
