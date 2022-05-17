c30284f7408f5e78404be44be0332cc9c1f6429e
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-05-17 19:55:59 +0530

    Update init.lua

--- nvim/lua/statusline/init.lua
+++ nvim/lua/statusline/init.lua
@@ -1,64 +1,96 @@
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+	return
+end
+local hide_in_width = function()
+	return vim.fn.winwidth(0) > 80
+end
 
-local function main()
+local diagnostics = {
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn" },
+	symbols = { error = "  ", warn = " " },
+	colored = false,
+	update_in_insert = false,
+	always_visible = true,
+}
+
+local function main_icon1()
   return [[ ]]
 end
 
--- stylua: ignore
-local colors = {
-  blue   = '#96CDFB',
-  cyan   = '#79dac8',
-  black  = '#302D41',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#96CDFB',
-  grey   = '#272536',
+local diff = {
+	"diff",
+	colored = false,
+	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+  cond = hide_in_width
 }
 
-local cat = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
-  },
+local mode = {
+	"mode",
+	fmt = function(str)
+		return " " .. str .. ""
+	end,
 }
 
-require('lualine').setup {
-  options = {
-    theme = cat,
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = {
-      { main, separator = { right = '' }, right_padding = 2 },
-    },
-    --lualine_b = {{'filename', separator = { right = '' } }},
-    lualine_b = {{ 'filename', path = 1, separator = { right = '' }, left_padding = 3 }},
-    lualine_y = {'mode'},
-    lualine_z = {
-      {'progress',separator = { left = '' }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {},
-  extensions = {},
+local filetype = {
+	"filetype",	
+	icons_enabled = true,
+	icon = nil,
 }
 
+local branch = {
+	"branch",
+	icons_enabled = true,
+	icon = "",
+}
 
+local location = {
+	"location",
+	padding = 2,
+}
+
+-- cool function for progress
+local progress = function()
+	local current_line = vim.fn.line(".")
+	local total_lines = vim.fn.line("$")
+	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+	local line_ratio = current_line / total_lines
+	local index = math.ceil(line_ratio * #chars)
+	return chars[index]
+end
+
+local spaces = function()
+	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+
+lualine.setup({
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "|" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "NvimTree", "Outline" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = {main_icon1},
+		lualine_b = {branch, diagnostics},
+		lualine_c = {{ 'filename', path = 1, separator = { right = '' }, left_padding = 3 }},
+		-- lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_x = { spaces, diff, "encoding", filetype },
+		lualine_y = { location },
+		lualine_z = { "progress" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename"},
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
+})
