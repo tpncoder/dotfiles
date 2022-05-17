ece55ef03a7ce46b53cedd771ff65cbcf536904d
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-05-17 19:50:12 +0530

    Update init.lua

--- nvim/lua/keymaps/init.lua
+++ nvim/lua/keymaps/init.lua
@@ -4,18 +4,22 @@ local buf_keymap = vim.api.nvim_buf_set_keymap
 
 vim.g.mapleader = " "
 
-keymap("n","<leader>q",":q", { noremap = true  })
-keymap("n","<leader>i",":PaqInstall", { noremap = true})
-keymap("n","<leader>h",":sp", { noremap = true })
-keymap("n","<leader>v",":vsp", { noremap = true })
+keymap("n","<leader>q",":q<cr>", { noremap = true  })
+keymap("n","<leader>i",":PackerInstall<cr>", { noremap = true})
+keymap("n","<leader>c",":PackerClean<cr>", { noremap = true})
+keymap("n","<leader>s",":PackerSync<cr>", { noremap = true})
+keymap("n","<leader>h",":sp<cr>", { noremap = true })
+keymap("n","<leader>v",":vsp<cr>", { noremap = true })
 keymap("n","<leader>op", ":NvimTreeToggle<cr>",  { noremap = true })
-keymap("n","<leader>w", ":WhichKey", { noremap = true })
+keymap("n","<leader>w", ":WhichKey<cr>", { noremap = true })
 keymap("n","<leader>tc",":Telescope colorscheme<cr>", { noremap = true })
 keymap("n","<leader>ff",":Telescope find_files<cr>", { noremap = true })
 keymap("n","<leader>fo",":Telescope oldfiles<cr>", { noremap = true })
 keymap("n","<leader>bm",":Telescope marks<cr>", { noremap = true })
 keymap("n","<leader>fw",":Telescope live_grep<cr>", { noremap = true })
-keymap("n","<C-d>",":Alpha", { noremap = true })
+keymap("n","<C-h>",":Alpha<cr>", { noremap = true })
+keymap("n","<leader>t", ":FloatermNew<cr>", { noremap = true })
+keymap("n","<leader>kt", ":FloatermKill<cr>", { noremap = true })
 
 vim.cmd[[nnoremap <C-d> :Alpha]]
 local keymap_g = {
