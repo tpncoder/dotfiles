b624ed07832622ec12f8a6a54665e90eb5ea0b1a
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-05-17 19:53:35 +0530

    Update init.lua

--- nvim/lua/packages/init.lua
+++ nvim/lua/packages/init.lua
@@ -1,64 +1,99 @@
-	--packages
-require "paq" {
-	  	"savq/paq-nvim",
-		'wbthomason/packer.nvim',
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+vim.cmd [[packadd packer.nvim]]
+--packages
+
+vim.notify = require("notify")
+
+return require('packer').startup(function()
+  	  	use "savq/paq-nvim"
+		use 'wbthomason/packer.nvim'
 	
 		--some shit
-	  	"nvim-telescope/telescope.nvim",
-	  	--"nvim-neo-tree/neo-tree.nvim",
-	  	"nvim-treesitter/nvim-treesitter",
-	 	"kyazdani42/nvim-tree.lua",
-	 	"kyazdani42/nvim-web-devicons",
-	  	"nvim-lua/plenary.nvim",
-	  	"folke/which-key.nvim",
-	
+	  	use "nvim-telescope/telescope.nvim"
+	  	use "nvim-neo-tree/neo-tree.nvim"
+		use "MunifTanjim/nui.nvim"
+	  	use "nvim-treesitter/nvim-treesitter"
+	 	use "kyazdani42/nvim-tree.lua"
+	 	use "kyazdani42/nvim-web-devicons"
+	  	use "nvim-lua/plenary.nvim"
+	  	use "folke/which-key.nvim"
+
 		--colorschemes
-	  	"catppuccin/nvim",
-	  	"arcticicestudio/nord-vim",
- 	  	"dracula/vim",
-	  	"folke/tokyonight.nvim",
-		'navarasu/onedark.nvim',
-		"ellisonleao/gruvbox.nvim",
-		'NTBBloodbath/doom-one.nvim',
-		'tomasiser/vim-code-dark',
+	  	use "catppuccin/nvim"
+	  	use "arcticicestudio/nord-vim"
+ 	  	use "dracula/vim"
+	  	use "folke/tokyonight.nvim"
+		use 'navarasu/onedark.nvim'
+		use "ellisonleao/gruvbox.nvim"
+		use 'NTBBloodbath/doom-one.nvim'
+		use 'tomasiser/vim-code-dark'
+		use 'rebelot/kanagawa.nvim'
 
 		--completion and lsp
-	  	"hrsh7th/vim-vsnip",
-	  	"hrsh7th/vim-vsnip",
-	  	"hrsh7th/vim-vsnip-integ",
-	  	"neovim/nvim-lspconfig",
-	  	"hrsh7th/cmp-nvim-lsp",
-	  	"hrsh7th/cmp-buffer",
-	  	"hrsh7th/cmp-path",
-	  	"hrsh7th/cmp-cmdline",	 
-	  	"hrsh7th/nvim-cmp",
-	  	"L3MON4D3/LuaSnip",
-	  	"saadparwaiz1/cmp_luasnip",
-	  	"williamboman/nvim-lsp-installer",
+	  	use "hrsh7th/vim-vsnip"
+	  	use "hrsh7th/vim-vsnip-integ"
+	  	use "neovim/nvim-lspconfig"
+	  	use "hrsh7th/cmp-nvim-lsp"
+	  	use "hrsh7th/cmp-buffer"
+	  	use "hrsh7th/cmp-path"
+	  	use "hrsh7th/cmp-cmdline" 
+	  	use "hrsh7th/nvim-cmp"
+	  	use "L3MON4D3/LuaSnip"
+	  	use "saadparwaiz1/cmp_luasnip"
+	  	use "williamboman/nvim-lsp-installer"
+		use {"folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons", config = function()
+			require("trouble").setup {}
+		end}
 
 		--icing of the cake
-	 	"rcarriga/nvim-notify",
-		"nvim-lualine/lualine.nvim",
-	  	'folke/which-key.nvim', 
-	 	'akinsho/bufferline.nvim',
-		--"glepnir/dashboard-nvim",
-	  	"karb94/neoscroll.nvim",
-		'feline-nvim/feline.nvim',
-	  	--'itchyny/lightline.vim',
-	  	'goolord/alpha-nvim',
-	  	"akinsho/toggleterm.nvim",
-		"noib3/nvim-cokeline",
+	 	use "rcarriga/nvim-notify"
+		use "nvim-lualine/lualine.nvim"
+	 	use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
+	        use {'romgrk/barbar.nvim', requires = {'kyazdani42/nvim-web-devicons'}}
+		--"glepnir/dashboard-nvim"
+	  	use "karb94/neoscroll.nvim"
+		use 'feline-nvim/feline.nvim'
+	  	--'itchyny/lightline.vim'
+	  	use 'goolord/alpha-nvim'
+	  	use "akinsho/toggleterm.nvim"
+		use "noib3/nvim-cokeline"
+		use 'kdheepak/tabline.nvim'
+		use 'alvarosevilla95/luatab.nvim'
+		use 'voldikss/vim-floaterm'
 
-	  	"lervag/vimtex",
-	  	"mattn/efm-langserver",
-	  	"pangloss/vim-javascript",
-	  	"MaxMEllon/vim-jsx-pretty",
-	  	"airblade/vim-gitgutter",
-	  	"tpope/vim-unimpaired",
-	  	"tpope/vim-surround",
-		'ellisonleao/glow.nvim',
-		"mhartington/formatter.nvim",
-  	  	"alvan/vim-closetag",
-	  	"jiangmiao/auto-pairs",
-		'Pocco81/TrueZen.nvim',
-			}
+	  	use "lervag/vimtex"
+	  	use "mattn/efm-langserver"
+	  	use "pangloss/vim-javascript"
+	  	use "MaxMEllon/vim-jsx-pretty"
+	  	use "airblade/vim-gitgutter"
+	  	use "tpope/vim-unimpaired"
+	  	use "tpope/vim-surround"
+		use 'ellisonleao/glow.nvim'
+		use "mhartington/formatter.nvim"
+  	  	use "alvan/vim-closetag"
+	  	use "jiangmiao/auto-pairs"
+		use 'Pocco81/TrueZen.nvim'
+        	use "lukas-reineke/indent-blankline.nvim"
+		use {'s1n7ax/nvim-terminal', config = function() vim.o.hidden = true 
+		require('nvim-terminal').setup() end,}
+		use "frabjous/knap"
+		use 'davidgranstrom/nvim-markdown-preview'
+end)
+
+
