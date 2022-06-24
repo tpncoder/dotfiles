ccf663927d692314841883120daf0d761442bd18
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-06-24 18:30:18 +0530

    Create others.lua

---
+++ nvim/lua/plugins/others.lua
@@ -0,0 +1,36 @@
+require'impatient'.enable_profile()
+require('lspkind').init({
+    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+    mode = 'symbol_text',
+    preset = 'codicons',
+
+    -- override preset symbols
+    -- default: {}
+    symbol_map = {
+      Text = "",
+      Method = "",
+      Function = "",
+      Constructor = "",
+      Field = "ﰠ",
+      Variable = "",
+      Class = "ﴯ",
+      Interface = "",
+      Module = "",
+      Property = "ﰠ",
+      Unit = "塞",
+      Value = "",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "פּ",
+      Event = "",
+      Operator = "",
+      TypeParameter = ""
+    },
+})
