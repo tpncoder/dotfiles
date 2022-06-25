a7f312466e346bd423d911c346a326317e5585e7
Tejas Protim Neog <92169576+tpncoder@users.noreply.github.com>
2022-06-25 15:53:29 +0530

    Update utils.lua

--- nvim/lua/core/utils.lua
+++ nvim/lua/core/utils.lua
@@ -3,7 +3,7 @@ vim.wo.number = true
 vim.cmd[[let g:dashboard_default_executive ="telescope"]]
 vim.cmd[[set mouse=a]]
 vim.cmd[[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]]
---vim.cmd[[autocmd FileType Alpha set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2]]
+vim.cmd[[autocmd FileType Alpha set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2]]
 vim.cmd[[:set cursorline]]
 
 local vim = vim
@@ -11,3 +11,4 @@ local opt = vim.opt
 --opt.foldmethod = "indent"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.cmd[[setlocal foldignore=]]
+vim.opt.fillchars:append { eob = " " }
